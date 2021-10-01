00b0fffe84691c61c25f5209a8fce0fb9c51e98c
Botond Attila Várhegyi <botond.varhegyi@stud.uni-corvinus.hu>
2021-10-01 21:58:55 +0200

    mentés commit

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace UserMaintenance
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -88,11 +89,22 @@ namespace UserMaintenance
             this.listBox1.Size = new System.Drawing.Size(337, 204);
             this.listBox1.TabIndex = 5;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(346, 77);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "fájlba írás";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -114,6 +126,7 @@ namespace UserMaintenance
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
