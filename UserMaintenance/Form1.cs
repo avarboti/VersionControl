a6edfe9dc457b3b7f6e91afd070628c22ac58404
avarboti <botond.varhegyi@stud.uni-corvinus.hu>
2021-10-08 11:38:01 +0200

    Format table

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -82,11 +82,79 @@ namespace UserMaintenance
             {
                 xlSheet.Cells[1, 1] = headers[0];
             }
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator)
+                    values[counter, 4] = "van";
+                else
+                    values[counter, 4] = "nincs";
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "=" + GetCell(counter + 2, 8) + "/" + GetCell(counter + 2, 7);
+                counter++;
+            }
+        }
+
+        private void FormatTable()
+        {
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+            int lastCoolumnID = xlSheet.UsedRange.Columns.Count;
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, lastCoolumnID));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+
+            Excel.Range tableRange = xlSheet.get_Range(GetCell(1, 1), GetCell(lastRowID, lastCoolumnID));
+            tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range firstColumn = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, 1));
+            firstColumn.Interior.Color = Color.LightYellow;
+            firstColumn.Font.Bold = true;
+
+            Excel.Range lastColumn = xlSheet.get_Range(GetCell(2, lastCoolumnID), GetCell(lastRowID, lastCoolumnID));
+            lastColumn.Interior.Color = Color.LightGreen;
+
+
 
         }
+
+            private string GetCell(int x, int y)
+            {
+                string ExcelCoordinate = "";
+                int dividend = y;
+                int modulo;
+
+                while (dividend > 0)
+                {
+                    modulo = (dividend - 1) % 26;
+                    ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                    dividend = (int)((dividend - modulo) / 26);
+                }
+                ExcelCoordinate += x.ToString();
+
+                return ExcelCoordinate;
+            }
+        }
+
+
+
      
 
 
 
     }
-}
+
