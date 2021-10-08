5e414dd090a77644b69797f2313bfe2932813ae0
avarboti <botond.varhegyi@stud.uni-corvinus.hu>
2021-10-08 11:20:51 +0200

    CreateExcel

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -17,6 +17,10 @@ namespace UserMaintenance
 
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
+
         public Form1()
         {
             InitializeComponent();
@@ -27,5 +31,41 @@ namespace UserMaintenance
         {
           Flats = context.Flats.ToList();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                
+                xlApp = new Excel.Application();
+
+             
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+             
+                xlSheet = xlWB.ActiveSheet;
+
+                
+               // CreateTable(); //
+
+               
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+     
+
     }
 }
