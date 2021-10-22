4d603bfcd0b0833cbd97666812277db278214c2f
avarboti <botond.varhegyi@stud.uni-corvinus.hu>
2021-10-22 11:34:46 +0200

    entities

--- var/Form1.cs
+++ var/Form1.cs
@@ -12,9 +12,12 @@ namespace var
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
