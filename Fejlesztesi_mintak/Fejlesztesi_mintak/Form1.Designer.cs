433329eab26946110f6d26a3dd35ecec95c2694b
avarboti <botond.varhegyi@stud.uni-corvinus.hu>
2021-11-10 11:17:28 +0100

    vége

--- Fejlesztesi_mintak/Fejlesztesi_mintak/Form1.Designer.cs
+++ Fejlesztesi_mintak/Fejlesztesi_mintak/Form1.Designer.cs
@@ -87,7 +87,7 @@ namespace Fejlesztesi_mintak
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 244);
+            this.label1.Location = new System.Drawing.Point(46, 255);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 4;
